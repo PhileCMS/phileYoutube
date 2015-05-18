c68a55a4a74827eb7a949253460e41ce656a2c69
Schlaefer <openmail+sourcecode@siezi.com>
2015-05-18 09:46:38 +0200

    allow additional optional query params in video-id string
    related to #1

--- Classes/Plugin.php
+++ Classes/Plugin.php
@@ -11,6 +11,10 @@ namespace Phile\Plugin\Phile\Youtube;
  */
 class Plugin extends \Phile\Plugin\AbstractPlugin implements \Phile\Gateway\EventObserverInterface {
 
+    protected $settings = [
+        'queryDefaults' => []
+    ];
+
     /**
      * the constructor
      */
@@ -22,16 +26,48 @@ class Plugin extends \Phile\Plugin\AbstractPlugin implements \Phile\Gateway\Even
     /**
      * generate video-embedding HTML
      *
-     * @param YoutubeVideoInfo $video
+     * @param string $string
      * @return string
      */
-    public function getVideoHtml(YoutubeVideoInfo $video) {
+    public function getVideoHtml($string) {
+        list($id, $query) = $this->parseQuery($string);
+
+        $options = [];
+        $options['query'] = $query;
+        $defaults = ['query' => $this->settings['queryDefaults']];
+        $options = array_replace_recursive($defaults, $options);
+
+        $video = new YoutubeVideoInfo($id);
+
         $title = '';
         if ($this->settings['show_title']) {
             $title = $this->getTitleHtml($video);
         }
-        $id = $video->getId();
-        return "<div class=\"{$this->settings['wrapper_class']}\"><iframe width=\"{$this->settings['video_width']}\" height=\"{$this->settings['video_height']}\" src=\"//www.youtube.com/embed/{$id}?rel=0\" frameborder=\"0\" allowfullscreen></iframe>{$title}</div>";
+
+        $query = http_build_query($options['query']);
+        return "<div class=\"{$this->settings['wrapper_class']}\"><iframe width=\"{$this->settings['video_width']}\" height=\"{$this->settings['video_height']}\" src=\"//www.youtube.com/embed/{$id}?{$query}\" frameborder=\"0\" allowfullscreen></iframe>{$title}</div>";
+    }
+
+    /**
+     * parse video id string for query params
+     *
+     * <id>?foo=bar&baz=zap becomes [<id>, ['foo' => 'bar', 'baz' => 'zap']]
+     *
+     * @param $string
+     * @return array
+     */
+    protected function parseQuery($string) {
+        $parts = parse_url($string);
+        if (!$parts || empty($parts['query'])) {
+            return [$string, []];
+        }
+
+        // ampersands in page content are escaped by markup (markdown) parser
+        $parts['query'] = str_replace('&amp;', '&', $parts['query']);
+
+        $query = [];
+        parse_str($parts['query'], $query);
+        return [$parts['path'], $query];
     }
 
     /**
@@ -56,32 +92,33 @@ class Plugin extends \Phile\Plugin\AbstractPlugin implements \Phile\Gateway\Even
         return $title;
     }
 
-	/**
-	 * event method
-	 * @param string $eventKey
-	 * @param null   $data
-	 *
-	 * @return mixed|void
-	 */
-	public function on($eventKey, $data = null) {
-		if ($eventKey === 'template_engine_registered') {
-			$youtube = new \Twig_SimpleFunction('youtube', function ($id) {
-				$video = new YoutubeVideoInfo($id);
-				return $this->getVideoHtml($video);
-			});
-			$data['engine']->addFunction($youtube);
-		} else if ($eventKey === 'after_parse_content') {
-			// store the starting content
-			$content = $data['content'];
-			// this parse happens after the markdown
-			// which means that the potential ID is wrapped
-			// in p tags
-			$regex = "/(<p>)(youtube)=(.*?)(<\/p>)/";
-			// add the modified content back in the data
-			$data['content'] = preg_replace_callback($regex, function($matches) {
-				$video = new YoutubeVideoInfo($matches[3]);
-				return $this->getVideoHtml($video);
-			}, $content);
-		}
-	}
+    /**
+     * event method
+     *
+     * @param string $eventKey
+     * @param null $data
+     *
+     * @return mixed|void
+     */
+    public function on($eventKey, $data = null) {
+        if ($eventKey === 'template_engine_registered') {
+            $youtube = new \Twig_SimpleFunction('youtube', function ($string) {
+                return $this->getVideoHtml($string);
+            });
+            $data['engine']->addFunction($youtube);
+        } else {
+            if ($eventKey === 'after_parse_content') {
+                // store the starting content
+                $content = $data['content'];
+                // this parse happens after the markdown
+                // which means that the potential ID is wrapped
+                // in p tags
+                $regex = "/(<p>)(youtube)=(.*?)(<\/p>)/";
+                // add the modified content back in the data
+                $data['content'] = preg_replace_callback($regex, function ($matches) {
+                    return $this->getVideoHtml($matches[3]);
+                }, $content);
+            }
+        }
+    }
 }
--- config.php
+++ config.php
@@ -8,5 +8,13 @@ return array(
 	'show_title' => true, // get the title
 	'title_tag' => 'h2', // wrap the title in this tag, can be false for none
 	'video_height' => 480, // standard height
-	'video_width' => 853 // standard width
+	'video_width' => 853, // standard width
+  /**
+   * additional player parameters
+   *
+   * see: https://developers.google.com/youtube/player_parameters#Parameters
+   */
+	'queryDefaults' => [
+		'rel' => 0
+    ],
 );
